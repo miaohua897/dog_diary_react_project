85a55e97330002284df280931ac9c05290ca02ba
miaohua897 <haijianhoubj@gmail.com>
2025-01-02 09:21:52 -0800

    updated

--- src/component/NewDiary.jsx
+++ src/component/NewDiary.jsx
@@ -4,12 +4,22 @@ import { useState } from "react";
 function NewDiary (){
 
     const [value,setValue] = useState('');
-    const [info,setInfo] =useState('');
+  
+    const today = new Date();
+    const [info,setInfo] =useState({
+        date:'',
+        value:''
+    });
+    const [date,setDate] = useState(today.toString());
 
     const handleSubmit=(e)=>{
 
         e.preventDefault();
-        setInfo(value);
+        const newobj ={...info};
+        newobj.date = date;
+        newobj.value =value;
+        setInfo(newobj);
+        // console.log(date,value,newobj,info);
         setValue('');
        
     }
@@ -17,12 +27,24 @@ function NewDiary (){
  const newvalue = e.target.value;
         setValue(newvalue);
     }
+    const handleDate=(e)=>{
+        const newdate = e.target.value;
+        setDate(newdate);
+    }
 
    return (
     <form onSubmit={handleSubmit}>
       <h2>New Diary</h2>
-      <p>{info}</p>
+      <p>{info.date}</p>
+      <p>{info.value}</p>
       <label>
+      <input
+        type = 'text'
+        style={{height:30,width:600}}
+        value={date}
+        onChange={handleDate}
+        >
+        </input>
         <input
         type = 'text'
         style={{height:300,width:600}}
