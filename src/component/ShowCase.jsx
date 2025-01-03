8e5e0d54e842b05a9a65e9b255334598c05e51c3
miaohua897 <haijianhoubj@gmail.com>
2025-01-02 16:51:30 -0800

    updated

--- src/component/ShowCase.jsx
+++ src/component/ShowCase.jsx
@@ -2,21 +2,23 @@ import dog from '../images/dog.png';
 
 const ShowCase=()=>{
      return (
-        <div>
+        <div className='showCase'>
             <h1>My diary</h1>
         <img 
         style={{height:300,width:300,borderRadius:'50%'}}
         src={dog}
         alt='my picture'
         ></img>
+        <div>
         <p>
             my favorite food is ...
-
         </p>
         <p>
         I love playing...
         </p>
         </div>
+        </div>
+
      
      )
 }
